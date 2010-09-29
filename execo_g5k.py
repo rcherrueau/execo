a284eb3918c928f9b5bdd0f3c0d02db9a90ac847
Matthieu Imbert <matthieu.imbert@inria.fr>
2010-09-29 06:33:44 +0200

    refactoring get_oar_job_nodes

--- execo_g5k.py
+++ execo_g5k.py
@@ -765,13 +765,12 @@ def get_oar_job_nodes(oar_job_id = None, site = None, connexion_params = None, t
             oar_job_id = os.environ['OAR_JOB_ID']
         else:
             raise ValueError, "no oar job id given and no OAR_JOB_ID environment variable found"
+    cmd = "while (oarstat -sj %(oar_job_id)i | grep Waiting) > /dev/null 2>&1 ; do sleep 5 ; done ; if (oarstat -sj %(oar_job_id)i | grep Running) > /dev/null 2>&1 ; then oarstat -pj %(oar_job_id)i | oarprint host -f - ; else false ; fi" % {'oar_job_id': oar_job_id}
     if site != None:
         if connexion_params == None:
             connexion_params = default_frontend_connexion_params
-        remote_cmd = "while (oarstat -sj %(oar_job_id)i | grep Waiting) > /dev/null 2>&1 ; do sleep 5 ; done ; if (oarstat -sj %(oar_job_id)i | grep Running) > /dev/null 2>&1 ; then oarstat -pj %(oar_job_id)i | oarprint host -f - ; else false ; fi" % {'oar_job_id': oar_job_id}
-        process = SshProcess(Host(site), remote_cmd, connexion_params = connexion_params, timeout = timeout)
+        process = SshProcess(Host(site), cmd, connexion_params = connexion_params, timeout = timeout)
     else:
-        cmd = "while (oarstat -sj %(oar_job_id)i | grep Waiting) > /dev/null 2>&1 ; do sleep 5 ; done ; if (oarstat -sj %(oar_job_id)i | grep Running) > /dev/null 2>&1 ; then oarstat -pj %(oar_job_id)i | oarprint host -f - ; else false ; fi" % {'oar_job_id': oar_job_id}
         process = Process(cmd, timeout = timeout)
     process.run()
     if process.ok():
