cc4bef421624105898662b95bf580fe3b7ba9a75
Laurent Pouilloux <lolo@pouilloux.org>
2013-01-15 13:15:14 +0100

    mise à jour du script pour trouver un free slots pour supprimer la dépendance XML

--- contrib/tools/find_free_slots.py
+++ contrib/tools/find_free_slots.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys, copy, json
+import sys, json
 import time as T, datetime as DT
 import logging as LOG, pprint as PP
-import lxml.etree as ET, matplotlib.pyplot as PLT, matplotlib.dates as MD
+import matplotlib.pyplot as PLT, matplotlib.dates as MD
 from optparse import OptionParser, OptionGroup
-import execo as EX, execo_g5k as EX5
+import execo.time_utils as ET
+import execo_g5k as EX5
 from execo_g5k.api_utils import APIConnexion,get_g5k_sites, get_g5k_hosts, get_site_clusters, get_cluster_site, get_cluster_hosts, _get_api_password
 from execo_g5k.config import default_frontend_connexion_params, g5k_configuration
 from execo_g5k.oargrid import default_frontend_connexion_params,get_oargridsub_commandline
@@ -14,7 +15,7 @@ from execo_g5k.oargrid import default_frontend_connexion_params,get_oargridsub_c
 class g5k_find_free_slots(object):
 	'''
 	This tools determine when the resources you need are available on Grid5000 
-	platform thanks to the analysis of Gantt diagram obtained form oarstat command   
+	platform thanks to the analysis of Gantt diagram obtained form API   
 	and can (optionally) make the reservation.
 	
 	Parameters are :
@@ -30,11 +31,10 @@ class g5k_find_free_slots(object):
 	'''
 	
 	def __init__(self,walltime,resources,vlan=None,oargridsub_opts='-t deploy',
-				weeks=1,auto=False,log=True,with_plots=False):
-		self.restime=T.strftime('%Y%m%d_%H%M%S',T.localtime())
+				weeks=1,auto=False,log=True,with_plots=False,outdir='.'):
+		self.restime=ET.format_date(ET.unixts_to_datetime(T.time()))
 		self.resources=resources
-		self.start_logger(log)
-		self.get_g5k_elements()		
+		self.start_logger(log)		
 		self.define_dates(walltime, weeks)
 		self.compute_hosts_planning()
 		
@@ -45,7 +45,7 @@ class g5k_find_free_slots(object):
 		self.choose_freeslot(self.windows_ok,auto)
 			
 		self.create_submissions(vlan,oargridsub_opts,auto)
-		self.logger.info('Done.\n')
+		self.logger.info('Done.')
 	
 	def start_logger(self,log):
 		self.logger = LOG.getLogger('execo')
@@ -55,52 +55,78 @@ class g5k_find_free_slots(object):
 			self.logger.setLevel(LOG.ERROR)
 		self.logger.info('Starting \033[1m%s\033[0m ...',self.__class__.__name__)
 	
-	def get_g5k_elements(self):
-		self.g5k_api=APIConnexion( "https://api.grid5000.fr/2.1",username = g5k_configuration.get('api_username'),
-                                password = _get_api_password())
+	def compute_hosts_planning(self):
+		self.g5k_api=APIConnexion( "https://api.grid5000.fr/2.1",
+                                password = _get_api_password(g5k_configuration.get('api_username')))
 		self.logger.info('Retrieving Grid5000 sites, clusters, hosts and reservations from API ...')
 		self.g5k_sites=get_g5k_sites()
-		sites=[]
-		if resources.has_key('grid5000.fr'):
-			sites=self.g5k_sites
+		self.sites=[]
+		if self.resources.has_key('grid5000.fr'):
+			self.sites=self.g5k_sites
 		else:
-			for resource in resources:
+			for resource in self.resources:
 				if resource in self.g5k_sites:
-					if resource not in sites:
-						sites.append(resource)
+					if resource not in self.sites:
+						self.sites.append(resource)
 				else:
 					site=get_cluster_site(resource)
-					if site not in sites:
-						sites.append(site)
-		
-		self.xml=ET.Element('platform', {'id': 'grid5000.fr'})
-		for site in sites:
-			el_site = ET.SubElement(self.xml,'site',attrib={'id':site})
-			clusters=get_site_clusters(site)
-			for cluster in clusters:
+					if site not in self.sites:
+						self.sites.append(site)
+		self.logger.info('Computing hosts planning ...')
+		self.hosts_plannings={}		
+		for site in self.sites:
+			self.hosts_plannings[site]={}		
+			for cluster in get_site_clusters(site):
+				self.hosts_plannings[site][cluster]={}
 				(_, content)=self.g5k_api.get('/grid5000/sites/'+site+'/clusters/'+cluster+'/status?reservations_limit=100')
-				hosts = json.loads(content)			
+				hosts = json.loads(content)
 				for host in hosts['items']:
 					if host['hardware_state'] != 'dead':
-						cluster_id, radical = host['node_uid'].split('-')
-						if el_site.find("cluster[@id='"+cluster_id+"']") is None:
-							el_cluster=ET.SubElement(el_site,'cluster', attrib={'id':cluster_id})
-						else:
-							el_cluster=el_site.find("cluster[@id='"+cluster_id+"']")
-						if el_cluster.find("host[@id='"+host['node_uid']+"']") is None:
-							el_host=ET.SubElement(el_cluster,'host', attrib={'id':host['node_uid']})
-						else:
-							el_host=el_cluster.find("host[@id='"+host['node_uid']+"']")
+						planning={'busy': [], 'free': []}
 						for job in host['reservations']:
-							ET.SubElement(el_host,'job', attrib={'id': str(job['batch_id']), 'start':str(job['start_time']), 'stop': str(job['start_time']+job['walltime'])})
-			self.logger.info(' - \033[1m%s\033[0m proceeded', site)
-			
-		tree = ET.ElementTree(element=self.xml)
-		tree.write('platform-'+self.restime+'.xml', pretty_print=True)
+							if job['queue']!='besteffort' and (job['start_time'],job['start_time']+job['walltime']) not in planning['busy']:
+								job['start_time']=max(job['start_time'],self.startdate_stamp)
+								planning['busy'].append((job['start_time'],job['start_time']+job['walltime']))
+						planning['busy'].sort()
+						if len(planning['busy'])>0:
+							if planning['busy'][0][0]>self.startdate_stamp:
+								planning['free'].append((self.startdate_stamp,planning['busy'][0][0])) 
+							for i in range(0,len(planning['busy'])-1):
+								planning['free'].append((planning['busy'][i][1],planning['busy'][i+1][0]))
+							if planning['busy'][len(planning['busy'])-1][1]<self.enddate_stamp:
+								planning['free'].append((planning['busy'][len(planning['busy'])-1][1],self.enddate_stamp))
+						else:
+							planning['free'].append((self.startdate_stamp,self.enddate_stamp))
+						
+						self.hosts_plannings[site][cluster][host['node_uid']]=planning
 		
+#		self.xml=ET.Element('platform', {'id': 'grid5000.fr'})
+#		for site in sites:
+#			el_site = ET.SubElement(self.xml,'site',attrib={'id':site})
+#			clusters=get_site_clusters(site)
+#			for cluster in clusters:
+#				(_, content)=self.g5k_api.get('/grid5000/sites/'+site+'/clusters/'+cluster+'/status?reservations_limit=100')
+#				hosts = json.loads(content)		
+#				for host in hosts['items']:
+#					if host['hardware_state'] != 'dead':
+#						cluster_id, radical = host['node_uid'].split('-')
+#						if el_site.find("cluster[@id='"+cluster_id+"']") is None:
+#							el_cluster=ET.SubElement(el_site,'cluster', attrib={'id':cluster_id})
+#						else:
+#							el_cluster=el_site.find("cluster[@id='"+cluster_id+"']")
+#						if el_cluster.find("host[@id='"+host['node_uid']+"']") is None:
+#							el_host=ET.SubElement(el_cluster,'host', attrib={'id':host['node_uid']})
+#						else:
+#							el_host=el_cluster.find("host[@id='"+host['node_uid']+"']")
+#						for job in host['reservations']:
+#							ET.SubElement(el_host,'job', attrib={'id': str(job['batch_id']), 'start':str(job['start_time']), 'stop': str(job['start_time']+job['walltime'])})
+#			self.logger.info(' - \033[1m%s\033[0m proceeded', site)
+#			
+#		tree = ET.ElementTree(element=self.xml)
+#		tree.write('platform-'+self.restime+'.xml', pretty_print=True)
 		
 	def define_dates(self,walltime, weeks):
-		self.startdate=(DT.datetime.now()+DT.timedelta(minutes=5))
+		self.startdate=(DT.datetime.now()+DT.timedelta(seconds=10))
 		self.startdate_stamp=int(T.mktime(self.startdate.timetuple()))
 		self.enddate=(DT.datetime.now()+ DT.timedelta(weeks=weeks,minutes=5))
 		self.enddate_stamp=int(T.mktime(self.enddate.timetuple()))	
@@ -110,33 +136,6 @@ class g5k_find_free_slots(object):
 		self.logger.info('From \033[1m%s\033[0m to \033[1m%s\033[0m', 
 			self.startdate.strftime("%Y-%m-%d %H:%M"), self.enddate.strftime("%Y-%m-%d %H:%M"))
 
-	def compute_hosts_planning(self):
-		self.logger.info('Computing hosts planning ...')
-		self.hosts_plannings={}		
-		
-		for site in self.xml.findall('site'):
-			self.hosts_plannings[site.get('id')]={}		
-			for cluster in site.findall('cluster'):
-				
-				self.hosts_plannings[site.get('id')][cluster.get('id')]={}
-				for host in cluster.findall('host'):
-					planning={'busy': [], 'free': []}
-					for job in host.iter('job'):
-						if (int(job.get('start')),int(job.get('stop'))) not in planning['busy']:  
-							planning['busy'].append((int(job.get('start')),int(job.get('stop'))))
-					planning['busy'].sort()
-					if len(planning['busy'])>0:
-						if planning['busy'][0][0]>self.startdate_stamp:
-							planning['free'].append((self.startdate_stamp,planning['busy'][0][0])) 
-						for i in range(0,len(planning['busy'])-1):
-							planning['free'].append((planning['busy'][i][1],planning['busy'][i+1][0]))
-						if planning['busy'][len(planning['busy'])-1][1]<self.enddate_stamp:
-							planning['free'].append((planning['busy'][len(planning['busy'])-1][1],self.enddate_stamp))
-					else:
-						planning['free'].append((self.startdate_stamp,self.enddate_stamp))
-					self.hosts_plannings[site.get('id')][cluster.get('id')][host.get('id')]=planning
-		
-		
 	def get_hosts_element(self,element):
 		element_hosts={}
 		if element=='grid5000.fr':
@@ -166,7 +165,7 @@ class g5k_find_free_slots(object):
 		return sorted(windows_limits)
 
 	def merge_slots(self,slots):
-		if len(slots)>0:
+		if len(slots)>1:
 			for i in range(len(slots)):
 				j=i+1
 				if j==len(slots)-1:
@@ -183,9 +182,9 @@ class g5k_find_free_slots(object):
 				if j==len(slots)-1:
 					break
 			
-			for i in range(len(slots)):
-				if (slots[i][1]-slots[i][0])<1:
-					slots.pop(i)
+#			for i in range(len(slots)):
+#				if (slots[i][1]-slots[i][0])<1:
+#					slots.pop(i)
 		
 	
 	def compute_freeslots(self):
@@ -261,6 +260,7 @@ class g5k_find_free_slots(object):
 		ax_all.set_xlim(self.startdate_stamp,self.enddate_stamp)
 		ax_all.set_ylim(0,n_res+1)
 
+		
 		i_res=0
 		for res_id, n_nodes in  self.resources.iteritems():
 			hosts=self.get_hosts_element(res_id)
@@ -299,10 +299,14 @@ class g5k_find_free_slots(object):
 		PLT.show()
 
 	def choose_freeslot(self,freeslots,autochoice):
+		i_test=0
 		for freeslot in freeslots:
 			if freeslot[0]==freeslot[1]:
 				freeslots.remove(freeslot)
-		
+			else:
+				freeslots[i_test]=(freeslot[0]+int(ET.timedelta_to_seconds(DT.timedelta(seconds=90))),freeslot[1])
+				i_test+=1	
+				
 		if len(freeslots)>0:
 			if not autochoice:
 				log='Available freeslots :'
@@ -314,7 +318,6 @@ class g5k_find_free_slots(object):
 				log+='\n x) Abort ...'
 				self.logger.info('%s', log)	
 				i_slot=raw_input('Choose your slot: ')
-				print i_slot
 				if i_slot=='x':
 					exit()
 				i_slot=int(i_slot)-1
@@ -327,6 +330,7 @@ class g5k_find_free_slots(object):
 			self.logger.error('There is not enough resources for your parameters, aborting ...')
 			exit()
 	
+
 	def create_submissions(self,vlan,oargridsub_opts,auto_reservation):
 		subs=[]
 		getlan=False
@@ -335,19 +339,18 @@ class g5k_find_free_slots(object):
 		
 		if self.resources.has_key('grid5000.fr'):
 			self.logger.info('Determining which sites to use for your reservation')
-			PP.pprint(self.resources)
 			total_nodes=0
 			sites_nodes={}
-			for site in self.xml.findall('site'):
-				if self.resources.has_key(site.get('id')):
-					sites_nodes[site.get('id')]=self.resources[site.get('id')]
+			for site in self.sites:
+				if self.resources.has_key(site):
+					sites_nodes[site]=self.resources[site]
 				else:
-					sites_nodes[site.get('id')]=0
+					sites_nodes[site]=0
 			while total_nodes != self.resources['grid5000.fr']:
 				max_site=''
 				max_nodes=0
-				for site in self.xml.findall('site'):
-					hosts=self.get_hosts_element(site.get('id'))
+				for site in self.sites:
+					hosts=self.get_hosts_element(site)
 					host_available=0
 					start=int(T.mktime(self.chosen_slot.timetuple()))
 					if start+self.duration <self.enddate_stamp:
@@ -358,41 +361,57 @@ class g5k_find_free_slots(object):
 						for freeslot in slots['free']:
 							if start>=freeslot[0] and stop<=freeslot[1]:
 								host_available+=1
-					if max_nodes<host_available-sites_nodes[site.get('id')]:
-						max_site=site.get('id')
-						max_nodes=host_available-sites_nodes[site.get('id')]
+					if max_nodes<host_available-sites_nodes[site]:
+						max_site=site
+						max_nodes=host_available-sites_nodes[site]
 				sites_nodes[max_site]+=1
 				total_nodes+=1
 			self.resources.clear()
 			for site, n_nodes in sites_nodes.iteritems():
 				if n_nodes>0:
 					self.resources[site]=n_nodes
+	
 		PP.pprint(self.resources)
-		for site in self.xml.findall('site'):
-			resources=''
+		for site in self.sites:
+			sub_resources=''
 			cluster_nodes=0
 			if getlan:
-				resources=vlan+'+'
+				sub_resources=vlan+'+'
 				getlan=False
+				self.lan_site=site
 			
-			for cluster in site.iter('cluster'):
-				if cluster.get('id') in self.resources:
-					resources+="{cluster=\\'"+cluster.get('id')+"\\'}/nodes="+str(self.resources[cluster.get('id')])+'+'
-					cluster_nodes+=self.resources[cluster.get('id')]
+			for cluster in get_site_clusters(site):
+				if cluster in self.resources:
+					sub_resources+="{cluster=\\'"+cluster+"\\'}/nodes="+str(self.resources[cluster])+'+'
+					cluster_nodes+=self.resources[cluster]
+					if site not in self.resources:
+						self.resources[site]=self.resources[cluster]
+					else:
+						self.resources[site]+=self.resources[cluster]
 				
-			if site.get('id') in self.resources:
-				resources+="nodes="+str(self.resources[site.get('id')]-cluster_nodes)+'+'
-				subs.append((EX5.OarSubmission(resources=resources[:-1]),site.get('id')))
+			if site in self.resources:
+				sub_resources+="nodes="+str(self.resources[site]-cluster_nodes)+'+'
+				subs.append((EX5.OarSubmission(resources=sub_resources[:-1]),site))
 		
-		self.logger.info('Reservation command: \n \033[1m%s\033[0m',get_oargridsub_commandline(subs,walltime=self.walltime,additional_options=oargridsub_opts,reservation_date=self.chosen_slot))
+		self.logger.info('Reservation command: \n\033[1m%s\033[0m',
+						get_oargridsub_commandline(subs,walltime=self.walltime,additional_options=oargridsub_opts,reservation_date=self.chosen_slot))
+		
 		if auto_reservation:			
-			EX5.oargridsub(subs,walltime=self.walltime,additional_options=oargridsub_opts,reservation_date=self.chosen_slot)
+			(self.oargrid_job_id, self.ssh_key)=EX5.oargridsub(subs,walltime=self.walltime,additional_options=oargridsub_opts,reservation_date=self.chosen_slot)
+			self.logger.info('Grid reservation done, oargridjob_id = %s',self.oargrid_job_id)
 		else:			
 			reservation=raw_input('Do you want me to do the reservation (y/n): ')
 			if reservation=='y':
-				(oargrid_job_id, ssh_key)=EX5.oargridsub(subs,walltime=self.walltime,additional_options=oargridsub_opts,reservation_date=self.chosen_slot)
-				self.logger.info('Grid reservation done, oargridjob_id = %s',oargrid_job_id)
-
+				(self.oargrid_job_id, self.ssh_key)=EX5.oargridsub(subs,walltime=self.walltime,additional_options=oargridsub_opts,reservation_date=self.chosen_slot)	
+				self.logger.info('Grid reservation done, oargridjob_id = %s',self.oargrid_job_id)
+	
+	def check_g5k_chart(self):
+		print 'coucou'
+	
+	
+	
+	
+	
 if __name__ == '__main__':
 	if len(sys.argv)>1:
 		
@@ -418,7 +437,8 @@ if __name__ == '__main__':
 		                action="store_true", 
 		                dest="plots", 
 		                default=False,
-		                help="don't print messages to stdout  (%default)")
+		                help="Draw Gantt chart before choosing freeslot")
+						
 		parser.add_option_group(optinout)
 		optreservation = OptionGroup(parser, "Reservation options", "Customize your grid5000 deployment and choose environment.")
 		optreservation.add_option("-r",
@@ -464,7 +484,7 @@ if __name__ == '__main__':
 		logger = LOG.getLogger("execo")
 		logger.setLevel(LOG.INFO)
 		
-		resources={'lille':150, 'chimint': 5, 'chicon': 2, 'lyon':10, 'stremi':100, 'grid5000.fr':800}
+		resources={'chimint': 5, 'lille': 10, 'stremi':10, 'grid5000.fr':100}
 		log='\n \033[1;31m -- WARNING : No arguments given, using demo parameters. -- \033[1;m \n'+PP.pformat(resources)
 		logger.warning('%s',log)
 		
