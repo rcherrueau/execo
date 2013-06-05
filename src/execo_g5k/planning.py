c29016c5df56985b243f1808fd481c5c3d1a32d0
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2013-06-06 00:18:42 +0200

    [execo_g5k] vmutils: renaming setup_cluster and state to deployment and actions, planning: fixing bugs in find_slots

--- src/execo_g5k/planning.py
+++ src/execo_g5k/planning.py
@@ -17,12 +17,13 @@
 # along with Execo.  If not, see <http://www.gnu.org/licenses/>
 
 """This module provides some tools to help you to plan your experiment on grid5000"""
-from pprint import pformat
+from pprint import pformat, pprint
 from operator import itemgetter
+from json import loads
 import time as T, datetime as DT, execo.time_utils as ET, math
 from execo.log import set_style, logger
-from execo import Host
-#from execo_g5k.config import default_connexion_frontend
+from execo import Host, Remote
+from execo_g5k.config import default_frontend_connexion_params
 
 import execo_g5k.api_utils as API
 try:
@@ -167,20 +168,20 @@ class Planning:
         return grid_planning
 
 
-#    def kavlan(self, startstamp, endstamp):
-#        """ Add to the planning"""
-#        if self.planning is None:
-#            self.planning = {}
-#        get_jobs = Remote('oarstat -J -f', [ Host(site+'.grid5000.fr') for site in self.sites], 
-#                      connexion_params = default_frontend_connexion_params ).run()
-#        for p in get_jobs.processes():
-#            site_jobs = loads(p.stdout())
-#            site_free = True
-#            for job_id, info in site_jobs.iteritems():
-#                if 'kavlan-global' in info['wanted_resources']:
-#                    site_free = False 
-#            if site_free:
-#                kavlan_site = p.host().address.split('.')[0]
+    def kavlan(self, startstamp, endstamp):
+        """ Add to the planning"""
+        if self.planning is None:
+            self.planning = {}
+        get_jobs = Remote('oarstat -J -f', [ Host(site+'.grid5000.fr') for site in self.sites], 
+                    connexion_params = default_frontend_connexion_params ).run()
+        for p in get_jobs.processes():
+            site_jobs = loads(p.stdout())
+            site_free = True
+            for job_id, info in site_jobs.iteritems():
+                if 'kavlan-global' in info['wanted_resources']:
+                    site_free = False 
+            if site_free:
+                kavlan_site = p.host().address.split('.')[0]
         
 
 
@@ -277,8 +278,6 @@ class Planning:
         
         slots.sort(key = itemgetter(0))
         
-        #self.merge_slots(slots)
-        
         g5k_clusters = API.get_g5k_clusters()
         for slot in slots:
             slot[2]['grid5000.fr'] = 0
@@ -299,16 +298,19 @@ class Planning:
 
         if mode == 'free':
             logger.info('Filtering slots with enough resources')
+
             slots_ok = {}
-            for slot, elements in self.slots.iteritems():
+            for slot in self.slots:
                 slot_ok = True
+                
                 if slot[1] - slot[0] >= duration:
-                    for element, n_nodes in elements.iteritems():
+                    for element, n_nodes in slot[2].iteritems():
                         if resources.has_key(element) and resources[element] > n_nodes:
                             slot_ok = False
                 if slot_ok:
-                    slots_ok[slot] = resources
+                    slots_ok = slot
             self.slots_ok = slots_ok
+                        
         elif mode == 'max':
             logger.info('Choosing slot with max nodes')
             max_slot = {}
@@ -320,166 +322,6 @@ class Planning:
                         max_slot = { slot : elements}
             self.max_slot = max_slot
 
-    def init_plots(self):
-        self.outsuffix = ET.format_date( self.startstamp ).replace('-', '').replace(' ','_').replace(':','')
-
-        self.colors = {}
-        self.colors['busy'] = '#CCCCCC'
-        rgb_colors = [(x[0]/255., x[1]/255., x[2]/255.) for x in \
-                                [(255., 122., 122.), (255., 204., 122.), (255., 255., 122.), (255., 246., 153.), (204., 255., 122.),
-                                (122., 255., 122.), (122., 255., 255.), (122., 204., 255.), (204., 188., 255.), (255., 188., 255.)]]
-        i_site = 0
-        for site in sorted(API.get_g5k_sites()):
-            self.colors[site] = rgb_colors[i_site]
-            i_cluster = 0
-            for cluster in sorted(API.get_site_clusters(site)):
-                min_index = self.colors[site].index(min(self.colors[site]))
-                color = [0., 0., 0.]
-                for i in range(3):
-                    color[i] = min(self.colors[site][i], 1.)
-                    if i == min_index:
-                        color[i] += i_cluster * 0.12
-                self.colors[cluster] = tuple(color)
-                i_cluster += 1
-            i_site += 1
-
-
-    def draw_gantt(self, show = False, save = False):
-        """ Draw the hosts planning for the element you ask (requires Matplotlib)"""
-        self.init_plots()
-        n_sites = len(self.planning.keys())
-        n_col = 2 if n_sites > 1 else 1
-        n_row = int(math.ceil(float(n_sites) / float(n_col)))
-
-        if self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=3)):
-            x_major_locator = MD.HourLocator(byhour = [9, 19])
-        elif self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=7)):
-            x_major_locator = MD.HourLocator(byhour = [9])
-        else:
-            x_major_locator = MD.AutoDateLocator()
-        xfmt = MD.DateFormatter('%d %b, %H:%M ')
-
-        fig = PLT.figure(figsize=(15, 5 * n_row), dpi=80)
-
-        i_site = 1
-        for site, clusters in self.planning.iteritems():
-            ax = fig.add_subplot(n_row, n_col, i_site, title = site.title())
-            ax.title.set_fontsize(18)
-            ax.xaxis_date()
-            ax.set_xlim(ET.unixts_to_datetime(self.startstamp), ET.unixts_to_datetime(self.endstamp))
-            ax.xaxis.set_major_formatter(xfmt)
-            ax.xaxis.set_major_locator(x_major_locator )
-            ax.xaxis.grid( color = 'black', linestyle = 'dashed' )
-            PLT.xticks(rotation = 15)
-            ax.set_ylim(0, 1)
-            ax.get_yaxis().set_ticks([])
-            ax.yaxis.label.set_fontsize(16)
-            n_hosts = 0
-            for hosts in clusters.itervalues():
-                n_hosts += len(hosts)
-            pos = 0.0
-            inc=1./n_hosts
-
-            ylabel = ''
-            for cluster, hosts in clusters.iteritems():
-                ylabel += cluster+' '
-                i_host = 0
-                for slots in hosts.itervalues():
-                    i_host +=1
-                    colors = {'free': self.colors[cluster], 'busy': self.colors['busy']}
-
-                    for kind in colors.iterkeys():
-                        for freeslot in slots[kind]:
-                            edate, bdate = [MD.date2num(item) for item in
-                                                            (ET.unixts_to_datetime(freeslot[1]), ET.unixts_to_datetime(freeslot[0]))]
-                            ax.barh(pos, edate - bdate , 1, left = bdate,
-                                    color = colors[kind],  edgecolor = 'none' )
-                    pos += inc
-                    if i_host == len(hosts):
-                        ax.axhline(y = pos, color = self.colors['busy'], linestyle ='-', linewidth = 1)
-            ax.set_ylabel(ylabel)
-            i_site += 1
-        fig.tight_layout()
-        if show:
-            PLT.show()
-        if save:
-            fname = 'gantt-'+self.outsuffix+'.png'
-            logger.info('Saving file %s ...', fname)
-            PLT.savefig (fname, dpi=300)
-
-    def draw_max_nodes(self, show = False, save = False):
-        """Draw the number of nodes available for the clusters (requires Matplotlib)"""
-        self.init_plots()
-        xfmt = MD.DateFormatter('%d %b, %H:%M ')
-        if self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=7)):
-            x_major_locator = MD.HourLocator(byhour = [9, 19])
-        elif self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=17)):
-            x_major_locator = MD.HourLocator(byhour = [9])
-        else:
-            x_major_locator = MD.AutoDateLocator()
-
-
-        max_nodes = {}
-        total_nodes = 0
-        slot_limits = []
-        total_list = []
-        for slot, elements in iter(sorted(self.slots.iteritems())):
-            slot_limits.append(slot[0])
-            slot_limits.append(slot[1]-1)
-            for element, n_nodes in elements.iteritems():
-                if element in API.get_g5k_clusters():
-                    if not max_nodes.has_key(element):
-                        max_nodes[element] = []
-                    max_nodes[element].append(n_nodes)
-                    max_nodes[element].append(n_nodes)
-                if element == 'grid5000.fr':
-                    total_list.append(n_nodes)
-                    total_list.append(n_nodes)
-                    if n_nodes > total_nodes:
-                        total_nodes = n_nodes
-        slot_limits.sort()
-        dates = [ET.unixts_to_datetime(ts) for ts in slot_limits]
-
-        datenums = MD.date2num(dates)
-
-        fig = PLT.figure(figsize=(15,10), dpi=80)
-#               PLT.title('Choose your slot')
-        ax = PLT.subplot(111)
-        ax.xaxis_date()
-        box = ax.get_position()
-        ax.set_position([box.x0-0.07, box.y0, box.width, box.height])
-        ax.set_xlim(ET.unixts_to_datetime(min(self.limits)), ET.unixts_to_datetime(max(self.limits)))
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Nodes available')
-        ax.set_ylim(0, total_nodes*1.1)
-        ax.axhline(y = total_nodes, color = '#000000', linestyle ='-', linewidth = 2, label = 'ABSOLUTE MAXIMUM')
-        ax.yaxis.grid(color='gray', linestyle='dashed')
-        ax.xaxis.set_major_formatter(xfmt)
-        ax.xaxis.set_major_locator(x_major_locator )
-        PLT.xticks(rotation = 15)
-
-
-        max_nodes_list = []
-
-        p_legend = []
-        p_rects = []
-        p_colors = []
-        for key, value in iter(sorted(max_nodes.iteritems())):
-            if key != 'grid5000.fr':
-                max_nodes_list.append(value)
-                p_legend.append(key)
-                p_rects.append(PLT.Rectangle((0, 0), 1, 1, fc = self.colors[key]))
-                p_colors.append(self.colors[key])
-        plots = PLT.stackplot(datenums, max_nodes_list, colors = p_colors)
-        PLT.legend(p_rects, p_legend, loc='center right', ncol = 1, shadow = True, bbox_to_anchor=(1.2, 0.5))
-
-
-        if show:
-            PLT.show()
-        if save:
-            fname = 'max_nodes-'+self.outsuffix+'.png'
-            logger.info('Saving file %s ...', fname)
-            PLT.savefig (fname, dpi=300)
 
 
 def get_first_cluster_available( clusters, walltime, n_nodes = 1):
@@ -494,15 +336,13 @@ def get_first_cluster_available( clusters, walltime, n_nodes = 1):
     for cluster in clusters:
         planning.find_slots('free', walltime, {cluster: n_nodes})
         start_min = 10**20
-        for start_stop in planning.slots_ok.keys():
-            if start_stop[0] < start_min:
-                start_min = start_stop[0]
-                first_slots[cluster] = start_stop
+        first_slots[cluster] = planning.slots_ok
 
-    first_slot = (10**20, 10**21)
+    pprint(first_slots)
+    first_slot = [10**20, 10**21]
     for cluster, slot in first_slots.iteritems():
         if slot[0] <= first_slot[0]:
-            first_slot = slot
+            first_slot = [slot[0], slot[1]]
             first_cluster = cluster
 
     return first_cluster, first_slot
@@ -531,3 +371,168 @@ def get_first_site_available( sites, walltime, n_nodes = 1):
             first_site = site
 
     return first_site, first_slot
+
+
+
+
+def set_colors(self):
+
+    self.outsuffix = ET.format_date( self.startstamp ).replace('-', '').replace(' ','_').replace(':','')
+
+    self.colors = {}
+    self.colors['busy'] = '#CCCCCC'
+    rgb_colors = [(x[0]/255., x[1]/255., x[2]/255.) for x in \
+                            [(255., 122., 122.), (255., 204., 122.), (255., 255., 122.), (255., 246., 153.), (204., 255., 122.),
+                            (122., 255., 122.), (122., 255., 255.), (122., 204., 255.), (204., 188., 255.), (255., 188., 255.)]]
+    i_site = 0
+    for site in sorted(API.get_g5k_sites()):
+        self.colors[site] = rgb_colors[i_site]
+        i_cluster = 0
+        for cluster in sorted(API.get_site_clusters(site)):
+            min_index = self.colors[site].index(min(self.colors[site]))
+            color = [0., 0., 0.]
+            for i in range(3):
+                color[i] = min(self.colors[site][i], 1.)
+                if i == min_index:
+                    color[i] += i_cluster * 0.12
+            self.colors[cluster] = tuple(color)
+            i_cluster += 1
+        i_site += 1
+
+
+def draw_gantt(self, show = False, save = False):
+    """ Draw the hosts planning for the element you ask (requires Matplotlib)"""
+    self.init_plots()
+    n_sites = len(self.planning.keys())
+    n_col = 2 if n_sites > 1 else 1
+    n_row = int(math.ceil(float(n_sites) / float(n_col)))
+
+    if self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=3)):
+        x_major_locator = MD.HourLocator(byhour = [9, 19])
+    elif self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=7)):
+        x_major_locator = MD.HourLocator(byhour = [9])
+    else:
+        x_major_locator = MD.AutoDateLocator()
+    xfmt = MD.DateFormatter('%d %b, %H:%M ')
+
+    fig = PLT.figure(figsize=(15, 5 * n_row), dpi=80)
+
+    i_site = 1
+    for site, clusters in self.planning.iteritems():
+        ax = fig.add_subplot(n_row, n_col, i_site, title = site.title())
+        ax.title.set_fontsize(18)
+        ax.xaxis_date()
+        ax.set_xlim(ET.unixts_to_datetime(self.startstamp), ET.unixts_to_datetime(self.endstamp))
+        ax.xaxis.set_major_formatter(xfmt)
+        ax.xaxis.set_major_locator(x_major_locator )
+        ax.xaxis.grid( color = 'black', linestyle = 'dashed' )
+        PLT.xticks(rotation = 15)
+        ax.set_ylim(0, 1)
+        ax.get_yaxis().set_ticks([])
+        ax.yaxis.label.set_fontsize(16)
+        n_hosts = 0
+        for hosts in clusters.itervalues():
+            n_hosts += len(hosts)
+        pos = 0.0
+        inc=1./n_hosts
+
+        ylabel = ''
+        for cluster, hosts in clusters.iteritems():
+            ylabel += cluster+' '
+            i_host = 0
+            for slots in hosts.itervalues():
+                i_host +=1
+                colors = {'free': self.colors[cluster], 'busy': self.colors['busy']}
+
+                for kind in colors.iterkeys():
+                    for freeslot in slots[kind]:
+                        edate, bdate = [MD.date2num(item) for item in
+                                                        (ET.unixts_to_datetime(freeslot[1]), ET.unixts_to_datetime(freeslot[0]))]
+                        ax.barh(pos, edate - bdate , 1, left = bdate,
+                                color = colors[kind],  edgecolor = 'none' )
+                pos += inc
+                if i_host == len(hosts):
+                    ax.axhline(y = pos, color = self.colors['busy'], linestyle ='-', linewidth = 1)
+        ax.set_ylabel(ylabel)
+        i_site += 1
+    fig.tight_layout()
+    if show:
+        PLT.show()
+    if save:
+        fname = 'gantt-'+self.outsuffix+'.png'
+        logger.info('Saving file %s ...', fname)
+        PLT.savefig (fname, dpi=300)
+
+def draw_max_nodes(self, show = False, save = False):
+    """Draw the number of nodes available for the clusters (requires Matplotlib)"""
+    self.init_plots()
+    xfmt = MD.DateFormatter('%d %b, %H:%M ')
+    if self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=7)):
+        x_major_locator = MD.HourLocator(byhour = [9, 19])
+    elif self.endstamp - self.startstamp <= ET.timedelta_to_seconds(DT.timedelta(days=17)):
+        x_major_locator = MD.HourLocator(byhour = [9])
+    else:
+        x_major_locator = MD.AutoDateLocator()
+
+
+    max_nodes = {}
+    total_nodes = 0
+    slot_limits = []
+    total_list = []
+    for slot, elements in iter(sorted(self.slots.iteritems())):
+        slot_limits.append(slot[0])
+        slot_limits.append(slot[1]-1)
+        for element, n_nodes in elements.iteritems():
+            if element in API.get_g5k_clusters():
+                if not max_nodes.has_key(element):
+                    max_nodes[element] = []
+                max_nodes[element].append(n_nodes)
+                max_nodes[element].append(n_nodes)
+            if element == 'grid5000.fr':
+                total_list.append(n_nodes)
+                total_list.append(n_nodes)
+                if n_nodes > total_nodes:
+                    total_nodes = n_nodes
+    slot_limits.sort()
+    dates = [ET.unixts_to_datetime(ts) for ts in slot_limits]
+
+    datenums = MD.date2num(dates)
+
+    fig = PLT.figure(figsize=(15,10), dpi=80)
+#               PLT.title('Choose your slot')
+    ax = PLT.subplot(111)
+    ax.xaxis_date()
+    box = ax.get_position()
+    ax.set_position([box.x0-0.07, box.y0, box.width, box.height])
+    ax.set_xlim(ET.unixts_to_datetime(min(self.limits)), ET.unixts_to_datetime(max(self.limits)))
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Nodes available')
+    ax.set_ylim(0, total_nodes*1.1)
+    ax.axhline(y = total_nodes, color = '#000000', linestyle ='-', linewidth = 2, label = 'ABSOLUTE MAXIMUM')
+    ax.yaxis.grid(color='gray', linestyle='dashed')
+    ax.xaxis.set_major_formatter(xfmt)
+    ax.xaxis.set_major_locator(x_major_locator )
+    PLT.xticks(rotation = 15)
+
+
+    max_nodes_list = []
+
+    p_legend = []
+    p_rects = []
+    p_colors = []
+    for key, value in iter(sorted(max_nodes.iteritems())):
+        if key != 'grid5000.fr':
+            max_nodes_list.append(value)
+            p_legend.append(key)
+            p_rects.append(PLT.Rectangle((0, 0), 1, 1, fc = self.colors[key]))
+            p_colors.append(self.colors[key])
+    plots = PLT.stackplot(datenums, max_nodes_list, colors = p_colors)
+    PLT.legend(p_rects, p_legend, loc='center right', ncol = 1, shadow = True, bbox_to_anchor=(1.2, 0.5))
+
+
+    if show:
+        PLT.show()
+    if save:
+        fname = 'max_nodes-'+self.outsuffix+'.png'
+        logger.info('Saving file %s ...', fname)
+        PLT.savefig (fname, dpi=300)
