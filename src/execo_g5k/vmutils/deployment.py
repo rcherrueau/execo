9032ba0aaa31187da3eee7b41800a3810bb69483
Laurent Pouilloux <lolo@pouilloux.org>
2013-10-01 12:00:11 +0200

    [execo_g5k] vmutils.deployment: - remove forced version of init-system-helpers - add a n_by_hosts distribution - reimplement write_placement_file

--- src/execo_g5k/vmutils/deployment.py
+++ src/execo_g5k/vmutils/deployment.py
@@ -134,7 +134,7 @@ class Virsh_Deployment(object):
     def install_packages(self, packages_list = None):
         """ Installation of packages on the nodes """
     
-        base_packages = 'uuid-runtime bash-completion qemu-kvm taktuk locate htop init-system-helpers=1.8'
+        base_packages = 'uuid-runtime bash-completion qemu-kvm taktuk locate htop init-system-helpers'
         logger.info('Installing usefull packages %s', set_style(base_packages, 'emph'))
         cmd = 'export DEBIAN_MASTER=noninteractive ; apt-get update && apt-get install -y --force-yes '+ base_packages
         install_base = self.fact.get_remote(cmd, self.hosts, connexion_params = {'user': 'root'}).run()        
@@ -447,39 +447,38 @@ class Virsh_Deployment(object):
     
     def write_placement_file(self):
         """ Generate an XML file with the VM deployment topology """
-        raise NotImplementedError
-        pass
-#        deployment = ETree.Element('vm5k')
-#        
-#        
-#        for vm in self.vms:
-#            host_info = vm['host'].address
-#            print host_info
-#            host_uid =   host_info.split('-')[0]+'-'+host_info.split('-')[1]
-#            cluster_uid = host_info.split('-')[0]
-#            site_uid = host_info.split('.')[1]
-#            print host_uid, cluster_uid, site_uid
-#            
-#            
-#            # FUCKING PYTHON 2.6 ....
-#            if deployment.find("./site[@id='"+site_uid+"']") is None:
-#                site = ETree.SubElement(deployment, 'site', attrib = {'id': site_uid})
-#            else:
-#                site = deployment.find("./site[@id='"+site_uid+"']")
-#            if site.find("./cluster/[@id='"+cluster_uid+"']") is None:
-#                cluster = ETree.SubElement(site, 'cluster', attrib = {'id': cluster_uid})
-#            else:
-#                cluster = site.find("./cluster/[@id='"+cluster_uid+"']")
-#            if cluster.find("./host/[@id='"+host_uid+"']") is None:
-#                host = ETree.SubElement(cluster, 'host', attrib = {'id': host_uid})
-#            else:
-#                host = cluster.find("./host/[@id='"+host_uid+"']")
-#            el_vm = ETree.SubElement(host, 'vm', attrib = {'id': vm['vm_id'], 'ip': vm['ip'], 'mac': vm['mac'], 
-#                        'mem': str(vm['mem_size']), 'cpu': str(vm['vcpus']), 'hdd': str(vm['hdd_size'])})
-#        
-#        f = open(self.outdir+'/placement.xml', 'w')
-#        f.write(prettify(deployment))
-#        f.close()
+       
+        deployment = ETree.Element('vm5k')
+        
+        
+        for vm in self.vms:
+            host_info = vm['host'].address
+            print host_info
+            host_uid =   host_info.split('-')[0]+'-'+host_info.split('-')[1]
+            cluster_uid = host_info.split('-')[0]
+            site_uid = host_info.split('.')[1]
+            print host_uid, cluster_uid, site_uid
+            
+            
+            # FUCKING PYTHON 2.6 ....
+            if deployment.find("./site[@id='"+site_uid+"']") is None:
+                site = ETree.SubElement(deployment, 'site', attrib = {'id': site_uid})
+            else:
+                site = deployment.find("./site[@id='"+site_uid+"']")
+            if site.find("./cluster/[@id='"+cluster_uid+"']") is None:
+                cluster = ETree.SubElement(site, 'cluster', attrib = {'id': cluster_uid})
+            else:
+                cluster = site.find("./cluster/[@id='"+cluster_uid+"']")
+            if cluster.find("./host/[@id='"+host_uid+"']") is None:
+                host = ETree.SubElement(cluster, 'host', attrib = {'id': host_uid})
+            else:
+                host = cluster.find("./host/[@id='"+host_uid+"']")
+            el_vm = ETree.SubElement(host, 'vm', attrib = {'id': vm['vm_id'], 'ip': vm['ip'], 'mac': vm['mac'], 
+                        'mem': str(vm['mem_size']), 'cpu': str(vm['vcpus']), 'hdd': str(vm['hdd_size'])})
+        
+        f = open(self.outdir+'/placement.xml', 'w')
+        f.write(prettify(deployment))
+        f.close()
 
         
     def distribute_vms(self, vms, mode = 'distributed', placement = None):    
@@ -528,6 +527,13 @@ class Virsh_Deployment(object):
                         hosts_vm[host.address] = []
                     vm['host'] = host
                     hosts_vm[host.address].append(vm['vm_id'])
+            elif mode == 'n_by_hosts':
+                i_vm = 0
+                for host in self.hosts:
+                    for i in range(len(vms)/len(self.hosts)):
+                        vms[i_vm]['host'] = host.address
+                        i_vm += 1
+                
         else: 
             clusters = []
             sites = []
