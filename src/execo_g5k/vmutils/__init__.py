7d830c17a0a870ede336fc29df602b243c018f8f
Laurent Pouilloux <lolo@pouilloux.org>
2013-12-16 15:36:41 +0100

    [execo_g5k] vmutils: add import of default_vm

--- src/execo_g5k/vmutils/__init__.py
+++ src/execo_g5k/vmutils/__init__.py
@@ -17,8 +17,9 @@
 # along with Execo.  If not, see <http://www.gnu.org/licenses/>
 
 
-from deployment import vm5k_deployment, distribute_vms, prettify, \
-    vm5k_deployment, get_oar_job_vm5k_resources, get_kavlan_ip_mac, get_max_vms, \
-    get_oargrid_job_vm5k_resources, default_vm, get_vms_slot
-from actions import install_vms, define_vms, create_disks, destroy_vms, list_vm, start_vms, wait_vms_have_started, create_disks_on_hosts
+from deployment import distribute_vms, prettify, vm5k_deployment, \
+    get_oar_job_vm5k_resources, get_kavlan_ip_mac, get_max_vms, \
+    get_oargrid_job_vm5k_resources, get_vms_slot
+from actions import default_vm, define_vms, install_vms, create_disks, destroy_vms, list_vm, \
+    start_vms, wait_vms_have_started, create_disks_on_hosts
 
