67253c7b14f96fdedfad927f1aae996f11e3f0ee
Matthieu Imbert <matthieu.imbert@inria.fr>
2014-01-21 15:34:04 +0100

    documentation/user_guide: chainput perf sample: propage job key with taktuk + blacklist graphite

--- doc/code_samples/g5k_chainput_perf.py
+++ doc/code_samples/g5k_chainput_perf.py
@@ -1,12 +1,14 @@
 from execo import *
 from execo_g5k import *
+import tempfile, shutil
 
 logger.info("compute resources to reserve")
-slots = compute_slots(get_planning(), 60*15)
+blacklisted = [ "graphite" ]
+slots = compute_slots(get_planning(), 60*15, excluded_elements = blacklisted)
 wanted = { "grid5000": 0 }
 start_date, end_date, resources = find_first_slot(slots, wanted)
-actual_resources = distribute_hosts(resources, wanted)
-job_specs = get_jobs_specs(actual_resources)
+actual_resources = distribute_hosts(resources, wanted, excluded_elements = blacklisted)
+job_specs = get_jobs_specs(actual_resources, excluded_elements = blacklisted)
 logger.info("try to reserve " + str(actual_resources))
 jobid, sshkey = oargridsub(job_specs, start_date,
                            walltime = end_date - start_date)
@@ -20,7 +22,10 @@ if jobid:
         nodes = get_oargrid_job_nodes(jobid)
         logger.info("got %i nodes" % (len(nodes),))
         conn_parms = default_oarsh_oarcp_params.copy()
-        conn_parms['keyfile'] = sshkey
+        tmpsshkey = tempfile.mktemp()
+        shutil.copy2(sshkey, tmpsshkey)
+        conn_parms['taktuk_options'] = ( "-s", "-S", "%s:%s" % (tmpsshkey, tmpsshkey))
+        conn_parms['keyfile'] = tmpsshkey
         broadcast1 = ChainPut(nodes, ["randomdata"], "/tmp/",
                               connection_params = conn_parms)
         broadcast2 = Put(nodes, ["randomdata"], "/tmp/",
