23689b4ed8416f709244697524f051fb5e46436e
Matthieu Imbert <matthieu.imbert@inria.fr>
2014-02-18 10:42:20 +0100

    [documentation] userguide: add box&whiskers + improve legend placement

--- doc/code_samples/g5k_tcp_congestion_process_results.py
+++ doc/code_samples/g5k_tcp_congestion_process_results.py
@@ -19,13 +19,19 @@ if __name__ == "__main__":
               [ result["params"]["num_flows"] for result in results ] }
         for tcp_congestion_control in
         [ result["params"]["tcp_congestion_control"] for result in results ] }
+    offset = -.25
     for tcp_congestion_control in arranged_results.keys():
+        plt.boxplot(arranged_results[tcp_congestion_control].values(),
+                    positions = [pos + offset for pos in arranged_results[tcp_congestion_control].keys() ])
         plt.plot(
             sorted(arranged_results[tcp_congestion_control].keys()),
             [ stats.scoreatpercentile(arranged_results[tcp_congestion_control][num_flows], 50)
               for num_flows in sorted(arranged_results[tcp_congestion_control].keys())],
             label = tcp_congestion_control, linewidth=0.5)
+        offset += .5
+    plt.xlim(0, max(arranged_results[tcp_congestion_control].keys()) + 1)
+    plt.xticks(arranged_results[tcp_congestion_control].keys())
+    plt.legend(loc = 'lower right')
     plt.xlabel('num flows')
     plt.ylabel('bandwith bits/s')
-    plt.legend()
     plt.savefig("g5k_tcp_congestion.png")
