549c7e1b4e0155ba9e1a843a95326cbc131c8326
Matthieu Imbert <matthieu.imbert@inria.fr>
2017-11-27 16:06:21 +0100

    [packaging] debian wheezy build env: add possibility to not sign the generated debs

--- wheezy_build/builddeb.sh
+++ wheezy_build/builddeb.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-GPGKEYID="$1"
-gpg --export-secret-key "$GPGKEYID" | vagrant ssh -c 'gpg --import -'
-vagrant ssh -c 'cd execo.git ; debuild --no-tgz-check -b -i -I -k"$GPGKEYID"'
+if [ "$1" == "UNSIGNED" ] ; then
+    vagrant ssh -c 'cd execo.git ; dpkg-buildpackage -us -uc'
+else
+    GPGKEYID="$1"
+    gpg --export-secret-key "$GPGKEYID" | vagrant ssh -c 'gpg --import -'
+    vagrant ssh -c 'cd execo.git ; debuild --no-tgz-check -b -i -I -k"$GPGKEYID"'
+fi
 TMPFILE=$(mktemp)
 vagrant ssh-config > "$TMPFILE"
 mkdir -p packages/
